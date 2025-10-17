b03ff4aa89bcd82a05d7ecf636f014bd393b214f
Richard Biju John <richardjohnn1100@gmail.com>
2025-10-17 14:46:42 +0530

    Create insertionsort.c

---
+++ insertionsort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void insr(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+void printarr(int arr[], int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n]; 
+    printf("Enter %d integers:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Original array: ");
+    printarr(arr, n);
+    insr(arr, n);
+    printf("Sorted array: ");
+    printArray(arr, n);
+    return 0;
+}
